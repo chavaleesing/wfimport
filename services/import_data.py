e54b7d1e331b2cd82ade41122714633149aca844
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-11-04 16:40:03 +0700

    [update] reconcile after insert file done

--- services/import_data.py
+++ services/import_data.py
@@ -10,21 +10,24 @@ def import_data_to_mysql(connection, cursor, file_path, filename):
     try:
         df = pd.read_csv(file_path, delimiter='|')
         df = df.replace(np.nan, None)
+        total_records = len(df)
         print(f"\n ----------- \n{file_path} loaded successfully from file.")
         table_name = "_".join(filename.split("_")[3:-2])
         placeholders = ', '.join(['%s'] * len(df.columns))
         columns = ', '.join(df.columns)
         sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
-        sum_reocrds = 0
+        commited_reocrds = 0
         batch_size = int(os.getenv("BATCH_SIZE", 10000))
-        # Insert data in batches
-        for start in range(0, len(df), batch_size):
+        for start in range(0, total_records, batch_size):
             batch_data = [tuple(row) for row in df[start:start+batch_size].values]
-            sum_reocrds += len(batch_data)
-            cursor.executemany(sql, batch_data)  # Batch insert
-            connection.commit()  # Commit each batch
-            print(f"{sum_reocrds} records imported successfully into the MySQL database.")
+            cursor.executemany(sql, batch_data)
+            connection.commit()
+            commited_reocrds += len(batch_data)
+            print(f"{commited_reocrds} records imported successfully into the MySQL database.")
         print(f"Data imported successfully into the MySQL database.")
+        if int(os.getenv("IS_RECONCILE", 0)):
+            if not commited_reocrds == total_records:
+                print(f"[ERROR-RECONCILATION] {commited_reocrds} == {total_records} on file: {filename}")
     except mysql.connector.Error as e:
         print(f"Error connecting to the database or inserting data: {e}")
     finally:
