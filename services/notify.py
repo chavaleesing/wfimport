12f788490442c2ac978eff98efff1ccb3f92b260
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-11-07 17:58:10 +0700

    [update][WIP] line notify

--- services/notify.py
+++ services/notify.py
@@ -3,7 +3,11 @@ import json
 import os
 
 
-def line_alert(text: str):
+def get_line_token() -> str:
+    # TODO get token by line api
+    return "wfDS0Lz4l6R8StClk2uWajzIN8IRLvmrl2b/5uug6ZdsSg6tx2TL7WfC0dgsvKWft3eoGhtdxKTDYTWPLXWEV3kYvJ8ZRv4FSnw1hsgQL4Mdg+8Eq23pXHI9+BR7goJvJeA8wpq3HHrQqZr6yP7h+o9PbdgDzCFqoOLOYbqAITQ="
+
+def line_alert(text: str) -> None:
     if not int(os.getenv("IS_ALERT", 0)):
         return
     url = "https://api.line.me/v2/bot/message/broadcast"
@@ -11,8 +15,10 @@ def line_alert(text: str):
     payload = json.dumps({
         "messages": [{"type": "text","text": text}]
     })
+    token = get_line_token()
     headers = {
-        'Authorization': 'Bearer wfDS0Lz4l6R8StClk2uWajzIN8IRLvmrl2b/5uug6ZdsSg6tx2TL7WfC0dgsvKWft3eoGhtdxKTDYTWPLXWEV3kYvJ8ZRv4FSnw1hsgQL4Mdg+8Eq23pXHI9+BR7goJvJeA8wpq3HHrQqZr6yP7h+o9PbdgDzCFqoOLOYbqAITQ=',
+        'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
     requests.request("POST", url, headers=headers, data=payload)
+
