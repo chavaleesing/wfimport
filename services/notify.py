5ba6e0058c1c0339d1bf1ac0f929fdbc98feb880
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-11-11 15:21:10 +0700

    [add] ms team alert

--- services/notify.py
+++ services/notify.py
@@ -22,3 +22,14 @@ def line_alert(text: str) -> None:
     }
     requests.request("POST", url, headers=headers, data=payload)
 
+def ms_alert(text: str) -> None:
+    if not int(os.getenv("IS_ALERT", 0)):
+        return
+    url = "https://krungthaigroup.webhook.office.com/webhookb2/161bc7af-4955-43d7-ae21-86a8b835c599@b8d867c0-b949-455c-95dd-cee5324ed815/IncomingWebhook/f19316efc73a4c07a494e81914d0499b/bf45723f-aa69-4c27-9222-9f204b590945/V2lurz3wzYWyNBRGqwtmE-1TNbvmg4PQ4VgJo-_z9UUIg1"
+
+    payload = json.dumps({
+        "topic": "Importing data from Data inno.",
+        "text": text
+    })
+    headers = {'Content-Type': 'application/json'}
+    requests.request("POST", url, headers=headers, data=payload)
