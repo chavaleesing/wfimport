46d292e6a1e1412ab1d45170d7898de6215efa94
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-12-04 15:15:44 +0700

    [add] update script

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from services.import_data import ImportData
+from services.update_data import UpdateData
 import os, sys
 
 path = os.path.dirname(os.path.abspath(__file__))  # NOQA
 sys.path.append(path)  # NOQA
 
-if len(sys.argv) == 2:
-    # To execute
-    ImportData().bulk_import(sys.argv[1])
+
+if len(sys.argv) == 3:
+    if sys.argv[1] == "import":
+        ImportData().bulk_import(sys.argv[2])
+    elif sys.argv[1] == "update":
+        UpdateData().bulk_update(sys.argv[2])
+    else:
+        print("Pls run script in format `python3 main.py <import/update> data/<folder_name>`")
 else:
-    print("Pls run script in format `python3 main.py data/company`")
+    print("Pls run script in format `python3 main.py <import/update> data/<folder_name>`")
