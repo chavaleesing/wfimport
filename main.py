db519006452b9e51f995ebbe0e041294da66d344
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-12-06 17:10:05 +0700

    [update] update data service

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ path = os.path.dirname(os.path.abspath(__file__))  # NOQA
 sys.path.append(path)  # NOQA
 
 
-if len(sys.argv) == 3:
-    if sys.argv[1] == "import":
-        ImportData().bulk_import(sys.argv[2])
+if len(sys.argv) == 2:
+    if sys.argv[1].startswith("data/"):
+        ImportData().bulk_import(sys.argv[1])
     elif sys.argv[1] == "update":
         UpdateData().update_data_to_mysql()
-        # UpdateData().bulk_update(sys.argv[2])
     else:
-        print("Pls run script in format `python3 main.py <import/update> data/<folder_name>`")
+        print("Pls run script in format `python3 main.py data/<folder_name>` OR `python3 main.py update`")
 else:
-    print("Pls run script in format `python3 main.py <import/update> data/<folder_name>`")
+    print("Pls run script in format `python3 main.py data/<folder_name>` OR `python3 main.py update`")
--- services/update_data.py
+++ services/update_data.py
@@ -1,16 +1,10 @@
-import os
 import gc
-import gzip
-import shutil
 import time
 import uuid
 import json
 
-from datetime import datetime
-
-import pandas as pd
 from services.notify import ms_alert
-from database import get_conn, close_conn
+from database import get_conn
 from services.helper import Helper
 
 from dotenv import dotenv_values
@@ -27,32 +21,37 @@ class UpdateData:
     def update_data_to_mysql(self) -> None:
         try:
             tbl_mains = env_vars["UPDATE_TABLES"].split(",")
-            tbl_main = tbl_mains[0]
-            tbl_name = tbl_main + "_temp"
-            if env_vars.get("UPDATE_CONDITION_MAPPING"):
-                update_condition_mapping = json.loads(env_vars.get("UPDATE_CONDITION_MAPPING"))
-                col_cond = f" {update_condition_mapping.get(tbl_main)} is NULL"
-            identifier_column = "id"
-            all_cols = Helper.get_cols_name(self.conn, self.cursor, tbl_name)
-            columns = [col for col in all_cols if col != identifier_column and (col.startswith("encrypted_") or col.startswith("hashed_"))]
-            set_clause = ', '.join([f"main.{col} = temp.{col}" for col in columns])
-            batch_size = int(env_vars["BATCH_SIZE"])
-            is_process_ids = True
-            while is_process_ids:
-                sql_ids = f"SELECT id FROM {tbl_main} WHERE {col_cond} limit {batch_size}"
-                self.cursor.execute(sql_ids)
-                ids = self.cursor.fetchall()
-                id_list = [id_tuple[0] for id_tuple in ids]
-                if not id_list:
-                    is_process_ids = False
+            for tbl_main in tbl_mains:
+                if Helper.is_exceed_time():
                     break
-                id_str = ', '.join([f"'{str(id)}'" for id in id_list])
-                sql = f"UPDATE {tbl_main} as main JOIN {tbl_name} as temp ON main.id = temp.id SET {set_clause} WHERE main.id IN ({id_str});"
-                self.cursor.execute(sql)
-                updated_rows = self.cursor.rowcount
-                self.conn.commit()
-                self.all_counts += updated_rows
-                ms_alert(f"[INFO][{self.unique_key}] Updating data table={tbl_main} | updated records={self.all_counts}")
+                tbl_name = tbl_main +   "_temp"
+                if env_vars.get("UPDATE_CONDITION_MAPPING"):
+                    update_condition_mapping = json.loads(env_vars.get("UPDATE_CONDITION_MAPPING"))
+                    print(update_condition_mapping.keys())
+                    col_cond = f" {update_condition_mapping.get(tbl_main)} is NULL"
+                identifier_column = "id"
+                all_cols = Helper.get_cols_name(self.conn, self.cursor, tbl_name)
+                columns = [col for col in all_cols if col != identifier_column and (col.startswith("encrypted_") or col.startswith("hashed_"))]
+                set_clause = ', '.join([f"main.{col} = temp.{col}" for col in columns])
+                batch_size = int(env_vars["BATCH_SIZE"])
+                is_process_ids = True
+                while is_process_ids:
+                    if Helper.is_exceed_time():
+                        break
+                    sql_ids = f"SELECT id FROM {tbl_main} WHERE {col_cond} limit {batch_size}"
+                    self.cursor.execute(sql_ids)
+                    ids = self.cursor.fetchall()
+                    id_list = [id_tuple[0] for id_tuple in ids]
+                    if not id_list:
+                        is_process_ids = False
+                        break
+                    id_str = ', '.join([f"'{str(id)}'" for id in id_list])
+                    sql = f"UPDATE {tbl_main} as main JOIN {tbl_name} as temp ON main.id = temp.id SET {set_clause} WHERE main.id IN ({id_str});"
+                    self.cursor.execute(sql)
+                    updated_rows = self.cursor.rowcount
+                    self.conn.commit()
+                    self.all_counts += updated_rows
+                    ms_alert(f"[INFO][{self.unique_key}] Updating data table={tbl_main} | updated_records={self.all_counts}")
         except Exception as e:
             raise e
         finally:
