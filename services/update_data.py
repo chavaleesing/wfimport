394450c036da51272f1349fe027844f2fb5c3a08
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2025-01-27 15:31:06 +0700

    [update] update data as chunk commit 1m

--- services/update_data.py
+++ services/update_data.py
@@ -1,11 +1,11 @@
-import gc
-import time
 import uuid
 import json
 
 from services.notify import ms_alert
-from database import get_conn
+from database import get_conn, close_conn
 from services.helper import Helper
+
+from datetime import datetime
 
 from dotenv import dotenv_values
 env_vars = dotenv_values(".env")
@@ -20,9 +20,10 @@ class UpdateData:
 
     def update_data_to_mysql(self) -> None:
         try:
+            if Helper.is_exceed_time(self.unique_key):
+                return
             tbl_mains = env_vars["UPDATE_TABLES"].split(",")
-            completed_tbl = []
-            partial_completed_tbl = []
+            completed_tbl = set()
             for tbl_main in tbl_mains:
                 ms_alert(f"[INFO][{self.unique_key}] Updating data table: {tbl_main}")
                 if Helper.is_exceed_time(self.unique_key):
@@ -40,28 +41,35 @@ class UpdateData:
                 while is_process_ids:
                     if Helper.is_exceed_time(self.unique_key):
                         break
-                    sql_ids = f"SELECT id FROM {tbl_main} WHERE {col_cond} limit {batch_size}"
+                    sql_ids = f"SELECT id FROM {tbl_main} WHERE {col_cond} limit 1000000;"
                     self.cursor.execute(sql_ids)
                     ids = self.cursor.fetchall()
-                    id_list = [id_tuple[0] for id_tuple in ids]
-                    if not id_list:
-                        completed_tbl.append(tbl_main)
+                    total_records = len(ids)
+                    print(f"[{datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')}][INFO][{self.unique_key}] total_records {total_records}")
+                    if total_records <= 0:
                         is_process_ids = False
                         break
-                    id_str = ', '.join([f"'{str(id)}'" for id in id_list])
-                    sql = f"UPDATE LOW_PRIORITY {tbl_main} as main JOIN {tbl_name} as temp ON main.id = temp.id SET {set_clause} WHERE main.id IN ({id_str});"
-                    self.cursor.execute(sql)
-                    updated_rows = self.cursor.rowcount
-                    self.conn.commit()
-                    self.all_counts += updated_rows
-                    print(f"[INFO][{self.unique_key}] Updating data table={tbl_main} | updated_records={self.all_counts}")
-                    if updated_rows == 0:
-                        partial_completed_tbl.append(tbl_main)
+                    update_count = 0
+                    for _, start in enumerate(range(0, total_records, batch_size)):
+                        id_to_update = [id_tuple[0] for id_tuple in ids[start:start+batch_size]]
+                        if not id_to_update:
+                            break
+                        id_str_to_update = ', '.join([f"'{str(id)}'" for id in id_to_update])
+                        sql = f"UPDATE LOW_PRIORITY {tbl_main} as main JOIN {tbl_name} as temp ON main.id = temp.id SET {set_clause} WHERE main.id IN ({id_str_to_update});"
+                        self.cursor.execute(sql)
+                        updated_rows = self.cursor.rowcount
+                        update_count += updated_rows
+                        self.all_counts += updated_rows
+                        print(f"[{datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')}][INFO][{self.unique_key}] Updating {updated_rows} on {tbl_main} | all_counts={self.all_counts}")
+                    if update_count == 0:
                         is_process_ids = False
                         break
-                time.sleep(1)
-            ms_alert(f"[INFO][{self.unique_key}] Completed update data completed_tbl={completed_tbl} | partial_completed_tbl={partial_completed_tbl} | updated_records={self.all_counts}")
+                    else:
+                        self.conn.commit()
+                        print(f"[{datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')}][INFO][{self.unique_key}] Updating data table={tbl_main} | updated_records={self.all_counts}")
+                        completed_tbl.add(tbl_main)
+            ms_alert(f"[INFO][{self.unique_key}] Completed update data completed_tbl={completed_tbl} | updated_records={self.all_counts}")
         except Exception as e:
             raise e
         finally:
-            gc.collect()
+            close_conn(self.conn)
